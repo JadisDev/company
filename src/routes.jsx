15245f055997c5f3cba3f82d19dc872d0755b4b7
Jadis da Silva Jale <jadis@rtdbrasil.org.br>
2020-11-19 17:30:53 -0300

    criado sistema de rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Login} />
-                <Route path="/teste" component={Login} />
+                <Route path="/signup" component={SignUp} />
             </Switch>
         </ BrowserRouter>
     )
