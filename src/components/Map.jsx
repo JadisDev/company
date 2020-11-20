b8dfbe023a28978f1471182eae71380c8197e2ae
Jadis Jale <jadis.jale@binddocs.com>
2020-11-20 03:17:14 -0300

    testes maps

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,29 +1,39 @@
+// https://www.youtube.com/watch?v=EouTckK3Cl8
+
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 const mapStyles = {
-  width: '94%',
-  height: '50%'
+    width: '94%',
+    height: '50%'
 };
 
 export class MapContainer extends Component {
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={
-          {
-            lat: -1.2884,
-            lng: 36.8233
-          }
-        }
-      />
-    );
-  }
+    render() {
+        return (
+            <Map
+                google={this.props.google}
+                zoom={14}
+                style={mapStyles}
+                initialCenter={
+                    {
+                        lat: -1.2884,
+                        lng: 36.8233
+                    }
+                }
+                yesIWantToUseGoogleMapApiInternals
+            >
+                <Marker
+                    onClick={this.onMarkerClick}
+                    name={'This is test name'}
+                />
+
+
+            </Map>
+        );
+    }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAKS1NPktBvX4_MRUfZeUawdLXWAFASLKE'
+    apiKey: 'AIzaSyAKS1NPktBvX4_MRUfZeUawdLXWAFASLKE'
 })(MapContainer);
